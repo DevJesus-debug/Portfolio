c3cdaede77204ca032740784ebf93259ad3ca91d
DevJesus-debug <jesusterrazasdev25@gmail.com>
2022-05-18 17:35:10 -0700

    working on content containers

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const wrap = document.querySelector(".wrap");
-let id = "";
+const hamburger = document.querySelector(".hamburger")
+const contentCont = document.querySelector(".content-cont")
+const navButtons = document.querySelectorAll(".content-nav button")
+const contactBtn = document.querySelector(".contact-btn")
+const projectsBtn = document.querySelector(".projects-btn")
+const aboutBtn = document.querySelector(".about-btn")
 
-
+hamburger.addEventListener("click", ()=>{
+  contentCont.classList.toggle("slide-up")
+  navButtons.forEach(button => {
+      button.classList.toggle("btn-slide")
+  });
+})
 
 function createBubbles(){
 
@@ -13,10 +23,9 @@ function createBubbles(){
           bubbles.style.top = Math.floor(Math.random() * 900) + "px";
           bubbles.style.left = Math.floor(Math.random() *375) + "px";
           wrap.appendChild(bubbles) 
-
-
         }
     }
+
 
 createBubbles()
 
